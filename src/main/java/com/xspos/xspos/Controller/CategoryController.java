d05f1c93c7354bf2b733ff1d64fa69b7d4230ecc
Xu Chen <chexu@visa.com>
2017-09-11 17:25:19 +0800

    bug fix: connection to db

--- src/main/java/com/xspos/xspos/Controller/CategoryController.java
+++ src/main/java/com/xspos/xspos/Controller/CategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Controller
 public class CategoryController {
---
+++ src/main/java/com/xspos/xspos/PersistenceContext.java
@@ -0,0 +1,20 @@
+package com.xspos.xspos;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceContext {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/XS_POS");
+        ds.setUsername("root");
+        ds.setPassword("2594");
+        return ds;
+    }
+}
